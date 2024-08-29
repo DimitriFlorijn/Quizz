b273bd8b7ade33fa81caa3c47152e9cc0b3b88ea
DimitriFlorijn <117824651+DimitriFlorijn@users.noreply.github.com>
2024-08-29 11:25:08 +0200

    imges

--- start.php
+++ start.php
@@ -5,18 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>the quizz itself</title>
-    <link rel="stylesheet" href=".css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
     </header>
+    <nav>
+        <p>u kunt altijd naar home</p>
+     <a href="index.php">Home</a>
+     
+  </nav>
 
     <div class="hoofd">
         <h2>kies moeilijkheid gradatie</h2>
-        <a href="start.php"><img src="werknemer.jpeg" alt="img"></a>
-        <a href="start.php"><img src="docent.jpeg" alt="img"></a>
-        <a href="start.php"><img src="student.jpeg" alt="img"></a>
-        <a href="start.php"><img src="new.jpeg" alt="img"></a>
+        <a href="index.php"><img src="werknemer.jpeg" alt="wrknemer"></a>
+        <a href="docent/docent.php"><img src="docent.jpeg" alt="docent"></a>
+        <a href="index.php"><img src="student.jpeg" alt="student"></a>
+        <a href="index.php"><img src="new.jpeg" alt="new tot software"></a>
+        <h2>iedere gradatie heeft 4 vraagen, klick op de foto van uw keus</h2>
         <?php
             
         ?>
